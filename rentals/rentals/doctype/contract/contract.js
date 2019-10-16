b88d24e2c7a4631d326cab86e2b4eb9e985dc399
joe kabucho <joekabucho2@gmail.com>
2019-10-16 12:59:34 +0300

    first

---
+++ rentals/rentals/doctype/contract/contract.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2018, Eric Baril and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Contract', {
+  refresh: function(frm) {
+    // Delivery Ticket
+    frm.add_custom_button(__('Delivery'), function() {
+      frappe.model.open_mapped_doc({
+        method: "rentals.rentals.doctype.contract.contract.make_delivery_note",
+        frm: frm
+      })
+    }, __("Make"));
+
+    // Pickup Ticket
+    frm.add_custom_button(__('Pickup'), function() {
+      frappe.model.open_mapped_doc({
+        method: "rentals.rentals.doctype.contract.contract.make_pickup_note",
+        frm: frm
+      })
+    }, __("Make"));
+
+    frm.page.set_inner_btn_group_as_primary(__("Make"));
+  }
+});
